9d279d46d33adbfaa41521174680256390e2a2e8
Kyle Hernandez <kylehernandez@kyles-macbook-air.local>
2018-12-23 22:16:15 -0600

    PrimeQs

--- NS-Elem-1/Controller/ViewController.swift
+++ NS-Elem-1/Controller/ViewController.swift
@@ -28,15 +28,10 @@ class ViewController: UIViewController {
     var answerUser = ""
     var isShow: Bool = false
     
-    var randomIndex = 0
-    var randomIndexA = 0
-    var randomNumA = 0
-    var randomNumB = 0
-    var randomMultiplier = 0
-    var firstNum = 0.00
-    var secondNum = 0.00
-    var thirdNum = 0
-    var fourthNum = 0
+    var numA = 0
+    var numB = 0
+    var numC = 0
+    var diffCat = 0
     
     let congratulateArray = ["Great Job", "Excellent", "Way to go", "Alright", "Right on", "Correct", "Well done", "Awesome","Give me a high five"]
     let retryArray = ["Try again","Oooops"]
@@ -57,58 +52,43 @@ class ViewController: UIViewController {
     }
     
     func askQuestion(){
-        randomIndexA = Int.random(in: 0...1)
-        switch randomIndexA{
+        diffCat = Int.random(in: 0...3)
+        switch diffCat{
         case 0:
-            randomMultiplier = (Int.random(in: 2...7))*2
-            randomNumA = Int.random(in: 5...15)
-            randomNumB = Int.random(in: 10...20)
-            while randomNumA == randomNumB{
-                randomNumA = Int.random(in: 5...15)
-                randomNumB = Int.random(in: 10...20)
-            }
-            firstNum = Double(randomNumA) / 2
-            secondNum = firstNum * Double(randomMultiplier)
-            thirdNum = randomNumB
-            fourthNum = thirdNum * randomMultiplier
+            numA = Int.random(in: 12...100)
+            numB = numA - 10
+            getSumOfPrimeNumBetween(startNum: numB, stopNum: numA)
+            questionLabel.text = "The sum of all prime numbers between \(numB) and \(numA)"
+            answerCorrect = String(numC)
+            numC = 0
         case 1:
-            randomMultiplier = (Int.random(in: 2...7))
-            randomNumA = Int.random(in: 5...15)
-            randomNumB = Int.random(in: 10...20)
-            while randomNumA == randomNumB{
-                randomNumA = Int.random(in: 5...15)
-                randomNumB = Int.random(in: 10...20)
-            }
-            firstNum = Double(randomNumA)
-            secondNum = Double(randomNumB)
-            thirdNum = randomNumA * randomMultiplier
-            fourthNum = randomNumB * randomMultiplier
-        default:
-            firstNum = 9.99
-            secondNum = 9.99
-            thirdNum = 99
-            fourthNum = 99
-        }
-        
-        randomIndex = Int.random(in: 0...3)
-        switch randomIndex{
-        case 0:
-            questionLabel.text = "  ____ is to \(secondNum) as \(thirdNum) is to \(fourthNum)."
-            answerCorrect = String(firstNum)
-        case 1:
-            questionLabel.text = "\(firstNum) is to ____ as \(thirdNum) is to \(fourthNum)."
-            answerCorrect = String(secondNum)
+            numA = (Int.random(in: 1...10))*10
+            questionLabel.text = "The largest prime number less than \(numA) is"
+            findLargestNumber()
+            answerCorrect = String(numA)
+            numA = 0
         case 2:
-            questionLabel.text = "\(firstNum) is to \(secondNum) as ____ is to \(fourthNum)."
-            answerCorrect = String(thirdNum)
+            numA = Int.random(in: 100...500)
+            while isPrime(num: numA){
+                numA = Int.random(in: 100...500)
+            }
+            questionLabel.text = "What is the smallest prime factor of \(numA)"
+            let numD = (primeFactors(n: numA))
+            let numE = numD.min()
+            answerCorrect = String(numE!)
         case 3:
-            questionLabel.text = "\(firstNum) is to \(secondNum) as \(thirdNum) is to ____."
-            answerCorrect = String(fourthNum)
+            numA = Int.random(in: 100...500)
+            while isPrime(num: numA){
+                numA = Int.random(in: 100...500)
+            }
+            questionLabel.text = "What is the largest prime factor of \(numA)"
+            let numD = (primeFactors(n: numA))
+            let numE = numD.max()
+            answerCorrect = String(numE!)
         default:
-            questionLabel.text = "999"
-            answerCorrect = "999"
+            questionLabel.text="999"
+            answerCorrect="999"
         }
-
     }
     
     @IBAction func showBtn(_ sender: Any) {
@@ -163,6 +143,40 @@ class ViewController: UIViewController {
     func randomTryAgain(){
         randomPick = Int(arc4random_uniform(2))
         readMe(myText: retryArray[randomPick])
+    }
+    func isPrime(num: Int) -> Bool {
+        if num < 2 {
+            return false
+        }
+        for i in 2..<num {
+            if num % i == 0 {
+                return false
+            }
+        }
+        return true
+    }
+    func getSumOfPrimeNumBetween(startNum: Int, stopNum: Int){
+        for i in (startNum+1)...(stopNum-1){
+            if isPrime(num: i) == true{
+                numC += i //is the sum of all prime number
+            }
+        }
+    }
+    func findLargestNumber(){
+        while isPrime(num: numA) == false{
+            numA -= 1
+        }
+    }
+    func primeFactors(n: Int) -> Array<Int> {
+        var n = n
+        var factors = [Int]()
+        for divisor in 2 ..< n {
+            while n % divisor == 0 {
+                factors.append(divisor)
+                n /= divisor
+            }
+        }
+        return factors
     }
 }
 
